0f47dfdbfcd84c02f543cb10f3561d32495fe4ff
Jeef TEMO <temokagj@3il.fr>
2024-11-29 11:12:26 +0100

    ajout de la fonction soustraction
    ajout de la fonction multiplication

--- sum.py
+++ sum.py
@@ -1,8 +1,13 @@
 # sum.py
 def add(a, b):
 return a + b
+def subtract(a, b):
+return a - b
+def multi(a, b):
+return a * b
 if __name__ == "__main__":
-num1 = 3
+num1 = 10
 num2 = 5
-print(f"La somme de {num1} et {num2} est : {add(num1,
+print(f"La somme de {num1} et {num2} est : {add(num1, num2)}")
+print(f"La différence de {num1} et {num2} est : {subtract(num1,
 num2)}")
